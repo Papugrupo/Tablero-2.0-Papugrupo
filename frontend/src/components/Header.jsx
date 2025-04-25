5cf96ef2321bb85a5740322f9578d55a8c3670a4
Yanko Acuña <yacuna20@alumnos.utalca.cl>
2025-04-25 11:57:58 -0400

    Cambio de ubicacion de boton de hamburguesa

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,18 +2,10 @@ import React from "react";
 
 export default function Header({ toggleSidebar }) {
   return (
-    <header className="bg-teal-700 text-white flex items-center justify-between px-6 py-1 shadow-md">
-      {/* Logo con imagen */}
-      <div>
-        <img
-          src="/images/utalca_icc.png" // Aquí debes colocar la ruta correcta de la imagen
-          alt="Logo"
-          className="h-18" // Ajusta el tamaño de la imagen según lo necesites
-        />
-      </div>
+    <header className="bg-teal-700 text-white flex items-center px-6 py-1 shadow-md">
 
       {/* Botón hamburguesa */}
-      <button
+            <button
         className="text-white focus:outline-none"
         onClick={toggleSidebar}
       >
@@ -27,6 +19,17 @@ export default function Header({ toggleSidebar }) {
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
+
+      {/* Logo con imagen */}
+      <div>
+        <img
+          src="/images/utalca_icc.png" // Aquí debes colocar la ruta correcta de la imagen
+          alt="Logo"
+          className="h-18" // Ajusta el tamaño de la imagen según lo necesites
+        />
+      </div>
+
+
     </header>
   );
 }
