2047409749fe33f40457b8ab0d85fb11de76130f
Gbustarr <gbustamante21@alumnos.utalca.cl>
2025-05-16 16:28:53 -0400

    feat(SeleccionarGrupo): Added image and modified style

--- frontend/src/views/SeleccionarGrupo.jsx
+++ frontend/src/views/SeleccionarGrupo.jsx
@@ -48,53 +48,59 @@ export default function SeleccionarGrupo() {
     <div className="min-h-screen bg-[#f4f9f9] text-[#1c2b2b]">
       <Header toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
       <Sidebar isOpen={sidebarOpen} closeSidebar={() => setSidebarOpen(false)} />
-      <main className="p-6 md:px-36 rounded-lg shadow-md">
+      <main className="p-6 md:px-36 rounded-lg">
         <h1 className="text-2xl font-bold mb-6">Hola</h1>
         <p className="mb-4">Para seguir con el proceso de registro, necesitas seleccionar o crear un grupo para ser asignado a él.</p>
         <h1 className="text-2xl font-bold mb-6">Gestión de Grupos</h1>
 
         {error && <p className="text-red-600 mb-4">{error}</p>}
 
-        <div className="flex flex-col gap-4  justify-center items-center ">
-        
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">Selecciona un grupo existente:</h2>
-          <select
-            className="p-2 border rounded w-full "
-            value={grupoSeleccionado}
-            onChange={handleSeleccionarGrupo}
-          >
-            <option value="">-- Selecciona un grupo --</option>
-            {grupos.map((grupo) => (
-              <option key={grupo.id} value={grupo.id}>
-                {grupo.nombre}
-              </option>
-            ))}
-          </select>
-        </section>
-
-       <section className="mb-8 justify-center ">
-            <h2 className="text-xl font-semibold mb-2">O</h2>
-        </section>
-
-        <section className="mb-8 justify-center">
-          <h2 className="text-xl font-semibold mb-2">Crear nuevo grupo</h2>
-          <form onSubmit={handleCrearGrupo} className="flex flex-col md:flex-row gap-4 items-start">
-            <input
-              type="text"
-              className="p-2 border rounded w-full"
-              placeholder="Nombre del grupo"
-              value={nombreNuevoGrupo}
-              onChange={(e) => setNombreNuevoGrupo(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
+        <div className="flex flex-col md:flex-row justify-between ">
+          <div className="flex flex-col gap-4  md:w-1/2 px-10">
+          
+          <section className="mb-8 ">
+            <h2 className="text-xl font-semibold mb-2">Selecciona un grupo existente:</h2>
+            <select
+              className="p-2 border rounded w-full "
+              value={grupoSeleccionado}
+              onChange={handleSeleccionarGrupo}
             >
-              Crear Grupo
-            </button>
-          </form>
-        </section>
+              <option value="">-- Selecciona un grupo --</option>
+              {grupos.map((grupo) => (
+                <option key={grupo.id} value={grupo.id}>
+                  {grupo.nombre}
+                </option>
+              ))}
+            </select>
+          </section>
+
+        <section className="mb-8 justify-center ">
+              <h2 className="text-xl font-semibold mb-2">O</h2>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold mb-2">Crear nuevo grupo</h2>
+            <form onSubmit={handleCrearGrupo} className="flex flex-row gap-4 items-start">
+              <input
+                type="text"
+                className="p-2 border rounded w-full"
+                placeholder="Nombre del grupo"
+                value={nombreNuevoGrupo}
+                onChange={(e) => setNombreNuevoGrupo(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
+              >
+                Crear
+              </button>
+            </form>
+          </section>
+          </div>
+          <div className="flex flex-col justify-center items-center  md:w-1/2">
+          <img src="./public/people.png" alt="Grupo" className=" mx-auto " />
+          
+          </div>
         </div>
 
         
